92942121bd3c6a42e3b64b62db6cf33ca5baab75
JessieStam <jessiestam@quicknet.nl>
2016-05-08 23:53:51 +0200

    Ninth commit

--- app/src/main/java/jstam/stamjessie_pset3/DBHelper.java
+++ app/src/main/java/jstam/stamjessie_pset3/DBHelper.java
@@ -1,7 +1,15 @@
 package jstam.stamjessie_pset3;
 
 /**
- * Created by Jessie on 08/05/2016.
+ * DBHelper.java
+ * Jessie Stam
+ * 10560599
+ *
+ * The database helper for the To-Do application.
+ *
+ * Although the two files are not actually linked, I hope you will take the time to check this file.
+ * I would like to get some feedback, since I still find it really hard to understand how to files
+ * interact with one another. Thanks in advance.
  */
 
 import android.content.ContentValues;
@@ -12,7 +20,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -25,6 +32,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        // create the table, add id and to-do items
         String query = "CREATE TABLE"+ TABLE+ " (_id"+" INTEGER PRIMARY KEY AUTOINCREMENT" + "todo TEXT)";
         db.execSQL(query);
     }
@@ -32,24 +40,40 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        // drop table and create it again when application is updated
         db.execSQL("DROP TABLE IF EXISTS " + TABLE);
         onCreate(db);
     }
 
+    /*
+     * Add to-do items to the list
+     */
     public void create(MainActivity addToList) {
+
+        // initialize database for writing
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put("todo", addToList.todo_item);
 
+        // add to-do item to the list and insert into table
+        values.put("todo", addToList.todo_item);
         db.insert(TABLE, null, values);
         db.close();
     }
 
+    /*
+     * Read through the database
+     */
     public ArrayList<HashMap<String, String>> read() {
+
+        // initialize database for reading
         SQLiteDatabase db = getReadableDatabase();
+
+        // select id and item from the table
         String query = "SELECT _id "+"todo "+ " FROM"+ TABLE;
         ArrayList<HashMap<String, String>> todo = new ArrayList<>();
         Cursor cursor = db.rawQuery(query, null);
+
+        // for every item in the list, read id and to-do
         if (cursor.moveToFirst()) {
             do {
                 HashMap<String, String> todo_list = new HashMap<>();
@@ -67,16 +91,30 @@ public class DBHelper extends SQLiteOpenHelper {
         return todo;
     }
 
+    /*
+     * Update database
+     */
     public void update(MainActivity addToList) {
+
+        // initialize database for writing
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
+
+        // add to-do item to list and add to the table
         values.put("todo", addToList.todo_item);
         db.update(TABLE, values, " id = ? ", new String[] {String.valueOf(addToList.id)});
         db.close();
     }
 
+    /*
+     * Delete item from the table
+     */
     public void delete(int id) {
+
+        // initialize database for writing
         SQLiteDatabase db = getWritableDatabase();
+
+        // delete to-do item from the table
         db.delete(TABLE, " _id = ? ", new String[] {String.valueOf(id)});
         db.close();
     }
--- app/src/main/java/jstam/stamjessie_pset3/MainActivity.java
+++ app/src/main/java/jstam/stamjessie_pset3/MainActivity.java
@@ -8,8 +8,21 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-
 import java.util.ArrayList;
+
+/**
+ * To-Do application
+ * Jessie Stam
+ * 10560599
+ *
+ * This application allows the uer to make a simple to-do list, to which items can be added and
+ * from which items can be selected and removed.
+ *
+ * Unfortunately I wasn't able to make the application function when I linked the DBHelper to my
+ * MainActivity, so the SQLite Database is not used since I found it more important for the
+ * application to function partially. However, I did finish the DBHelper, so I hope that you will
+ * take that into consideration. Thanks in advance.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     String finished = "finished";
     String unfinished = "unfinished";
     Integer id = 0;
+    //DBHelper storyName = new DBHelper(InputStream);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 (this, android.R.layout.simple_list_item_1, todo_list);
         currentColor = unfinished;
 
+
         /*
          * set onclick listener for ListView items to check/uncheck them
          */
@@ -50,18 +65,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                // if item is selected, change color to gray
                 if (currentColor.equals(unfinished)) {
                     screen_list.getChildAt(position).setBackgroundColor(Color.GRAY);
                     currentColor = finished;
                 }
+                // if item is not selected, change color back to white
                 else if (currentColor.equals(finished)) {
                     screen_list.getChildAt(position).setBackgroundColor(Color.WHITE);
                     currentColor = unfinished;
                 }
             }
         });
+
+        /*
+         * set long click listener for removing items
+         */
+        screen_list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View string, int position, long id) {
+
+                // remove the item at the touched position and update data
+                todo_list.remove(position);
+                todoAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
+    /*
+     * Save data for when screen is rotated or application is shut down
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -74,6 +108,9 @@ public class MainActivity extends AppCompatActivity {
         outState.putStringArrayList("todo_list", todo_list_save);
     }
 
+    /*
+     * Restore data after screen is rotated or application is restarted
+     */
     @Override
     public void onRestoreInstanceState(Bundle saveInstanceState) {
         super.onRestoreInstanceState(saveInstanceState);
@@ -83,6 +120,9 @@ public class MainActivity extends AppCompatActivity {
         todo_list = saveInstanceState.getStringArrayList("todo_list");
     }
 
+    /*
+     * Adds an item to the list
+     */
     public void addToList(View view) {
 
         // use adapter to put todo_list information to screen_list
@@ -96,9 +136,10 @@ public class MainActivity extends AppCompatActivity {
         // refresh ListView
         todoAdapter.notifyDataSetChanged();
 
+        // update id for to do item
         id = id + 1;
 
-        // write some code that cleans the EditText
-
+        // clear the input line after text is added
+        user_input.getText().clear();
     }
 }
